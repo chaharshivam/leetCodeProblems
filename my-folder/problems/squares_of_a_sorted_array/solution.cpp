38bee127a6e445f3078d459ac69f56c6f1e6769b
Shivam Chahar <shivam.chahar@gmail.com>
2021-04-10 07:43:04 +0000

    Sync LeetCode submission - Squares of a Sorted Array (cpp)

---
+++ my-folder/problems/squares_of_a_sorted_array/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+         for (int i = 0; i < nums.size(); i++)
+        nums[i] = nums[i] * nums[i];
+ 
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};

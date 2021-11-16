41a854497deee88d5219b032b1ee4253b7f424f2
Shivam Chahar <shivam.chahar@gmail.com>
2021-11-16 18:42:32 +0000

    Sync LeetCode submission - Concatenation of Array (cpp)

--- my-folder/problems/concatenation_of_array/solution.cpp
+++ my-folder/problems/concatenation_of_array/solution.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        int* ans = new int[2*(nums.size())];
-        for(int i = 0; i<nums.size(); i++){
-            ans[i] = nums[i];
-            ans[i+(nums.size())] = nums[i];
+        int i = 0;
+        int size = nums.size();
+        while(i<size){
+            nums.push_back(nums[i]);
+            i++;
         }
-        vector<int> v;
-        for(int i = 0; i<(2*nums.size()); i++){
-            v.push_back(ans[i]);
-        }
-        return v;
+        return nums;
     }
 };

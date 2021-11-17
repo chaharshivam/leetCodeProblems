090ecb563bac255be5ef6b5c3461b36d4fcd7e73
Shivam Chahar <shivam.chahar@gmail.com>
2021-11-17 19:17:26 +0000

    Sync LeetCode submission - Intersection of Two Arrays (cpp)

--- my-folder/problems/intersection_of_two_arrays/solution.cpp
+++ my-folder/problems/intersection_of_two_arrays/solution.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
+        bool binarySearch(vector<int>& arr,int start,int end,int target){
+    for(;start<=end;){
+        int mid = (start+end)/2;
+        if(arr[mid] == target){
+            return true;
+        }
+        if(target<arr[mid]){
+            end = mid-1;
+        }
+        if(target>arr[mid])
+        start = mid+1;
+    }
+    return false;
+}
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> s1;
-        unordered_set<int> s2;
-        unordered_set<int> s3;
-        for(auto it = nums1.begin(); it!=nums1.end(); it++){
-            s1.insert(*it);
-        }
-        for(auto it = nums2.begin(); it!=nums2.end(); it++){
-            s2.insert(*it);
-        }
-        for(auto it = s1.begin(); it!=s1.end(); it++){
-            if(s2.count(*it) > 0){
-                s3.insert(*it);
+         sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int> ans;
+        auto it = nums1.begin();
+        for(; it!=nums1.end(); it++){
+             bool res = binarySearch(nums2,0,nums2.size()-1,*it);
+            if(!res)
+                continue;
+            if(ans.size()==0){
+                ans.push_back(*it);
+                continue;
             }
+            bool res1 = binarySearch(ans,0,ans.size()-1,*it);
+            if(res1)
+                continue;
+            ans.push_back(*it);
         }
-        vector<int> v;
-        for(auto it = s3.begin(); it!=s3.end(); it++){
-            v.push_back(*it);
-        }
-        return v;
+        return ans;
+            
     }
 };

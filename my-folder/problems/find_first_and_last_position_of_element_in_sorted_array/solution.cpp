44841509e668f54f332ed49876e4942ce76b872c
Shivam Chahar <shivam.chahar@gmail.com>
2022-07-25 17:49:44 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (cpp)

--- my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
+++ my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
@@ -1,13 +1,44 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        auto it1 = find(nums.begin(), nums.end(), target);
-        if(it1 == nums.end())
+        vector<int> res;
+        int start = 0;
+        int end = nums.size() - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(nums.at(mid) == target){
+                if(mid == 0 || nums.at(mid - 1) != target){
+                    res.push_back(mid);
+                    break;
+                }
+                else
+                    end = mid - 1;
+            }
+            else if(nums.at(mid) > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        start = 0;
+        end = nums.size() - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(nums.at(mid) == target){
+                if(mid == nums.size() - 1 || nums.at(mid + 1) != target){
+                    res.push_back(mid);
+                    break;
+                }
+                else
+                    start = mid + 1;
+            }
+            else if(nums.at(mid) > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        if(res.size() == 0)
             return {-1,-1};
-        int index1 = it1 - nums.begin();
-        auto it2 = find(nums.rbegin(),nums.rend(), target);
-        int index2 = (nums.rend() - 1) - it2;
-        return {index1, index2};
-        
+        else
+            return res;
     }
 };

375cc2a2c7f319472e1bf2173ced208b48a18a19
Shivam Chahar <shivam.chahar@gmail.com>
2022-08-15 19:42:40 +0000

    Sync LeetCode submission - Roman to Integer (cpp)

---
+++ my-folder/problems/roman_to_integer/solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans=0;
+        map<char,int> mp;
+        
+        mp['I']=1;mp['V']=5;mp['X']=10;mp['L']=50;mp['C']=100;mp['D']=500;mp['M']=1000;
+             
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='I' && s[i+1]=='V'){
+                ans+=4;
+                i++;
+            }
+            else if(s[i]=='I' && s[i+1]=='X'){
+                ans+=9;
+                i++;
+            }
+            else if(s[i]=='X' && s[i+1]=='L'){
+                ans+=40;
+                i++;
+            }
+            else if(s[i]=='X' && s[i+1]=='C'){
+                ans+=90;
+                i++;
+            }
+            else if(s[i]=='C' && s[i+1]=='D'){
+                ans+=400;
+                i++;
+            }
+            else if(s[i]=='C' && s[i+1]=='M'){
+                ans+=900;
+                i++;
+            }
+            else{
+                ans+=mp[s[i]];
+            }
+                        
+            
+        }
+        
+        return ans;
+    }
+};

5e36ea307d7311a5477797429aaba95e9f883cff
Shivam Chahar <shivam.chahar@gmail.com>
2022-08-23 18:02:15 +0000

    Sync LeetCode submission - Palindrome Linked List (cpp)

---
+++ my-folder/problems/palindrome_linked_list/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> v;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+        auto it = v.begin();
+        auto et = v.end()-1;
+        while(it<=et){
+            if(*it!=*et)
+                return false;
+            it++;
+            et--;
+        }
+        return true;
+    }
+};

9baf40dc2b63c82e35fc9e6b5371f98e7de11fbe
Shivam Chahar <shivam.chahar@gmail.com>
2022-09-30 11:33:38 +0000

    Sync LeetCode submission - The Skyline Problem (python3)

---
+++ my-folder/problems/the_skyline_problem/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getSkyline(self, buildings):  
+        line=sorted((pos,tag,height,counterpart) for l,r,h in buildings 
+                    for pos,tag,height,counterpart in [(l,'l',-h,r),(r,'r',-h,l)])
+        prev_h,skyline,h_index=0,[],[(0,1<<32)]
+        for p,t,h,c in line:
+            if t=='l':
+                heappush(h_index,(h,c))
+            while h_index[0][1]<=p:
+                heappop(h_index)
+            h,_=h_index[0]
+            if h!=prev_h:
+                skyline.append((p,-h))
+                prev_h=h
+        return skyline
+        

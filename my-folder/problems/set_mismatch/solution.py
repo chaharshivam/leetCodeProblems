d11e545b838bc3e886157c8659d837d61c4c0d24
Shivam Chahar <shivam.chahar@gmail.com>
2022-10-23 07:00:59 +0000

    Sync LeetCode submission - Set Mismatch (python3)

---
+++ my-folder/problems/set_mismatch/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        l, s = len(nums), sum(set(nums))
+        l = l * ( 1 + l ) // 2
+        return [sum(nums) - s, l - s]
